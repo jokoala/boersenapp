2646dbbdcdd19e72e48fa8f259dbbf57f95a3d61
Johannes Kolb <johannes.kolb@gmx.net>
2013-08-12 22:10:28 +0200

    Initial check-in of module BoersenApp

---
+++ BoersenApp/MainWindow.cs
@@ -0,0 +1,35 @@
+using System;
+using Gtk;
+
+public partial class MainWindow: Gtk.Window
+{	
+	uint m_context_id;
+
+	public MainWindow (): base (Gtk.WindowType.Toplevel)
+	{
+		Build ();
+		m_context_id= statusbar1.GetContextId("MainWindow");
+	}
+
+	protected void notify (String message)
+	{
+		statusbar1.Push (m_context_id, message);
+	}
+	
+	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
+	{
+		Application.Quit ();
+		a.RetVal = true;
+	}
+
+	protected void OnComboboxentry1Changed (object sender, EventArgs e)
+	{
+		notify(String.Format ("Entry changed to: {0}", comboboxentry1.Active));
+	}	
+
+	protected void OnQuitActionActivated (object sender, EventArgs e)
+	{
+		Application.Quit ();
+	}
+
+}
